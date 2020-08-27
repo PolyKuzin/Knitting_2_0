53cdeca3247cdf6703731c57ccda90942b14b364
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-08-27 18:49:55 +0300

    Initial Commit

---
+++ Knitting_2_0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knitting_2_0
+//
+//  Created by Павел Кузин on 27.08.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Knitting_2_0Tests/Knitting_2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Knitting_2_0Tests.swift
+//  Knitting_2_0Tests
+//
+//  Created by Павел Кузин on 27.08.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import XCTest
+@testable import Knitting_2_0
+
+class Knitting_2_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
