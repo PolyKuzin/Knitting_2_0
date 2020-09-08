2df20e45d7846a32ae9822f22b5a22f13b5ff807
Pavel Kuzin <pakuzin@edu.hse.ru>
2020-09-08 23:44:24 +0300

    Preparations for entry

--- Knitting_2_0/Views/EntryVC.swift
+++ Knitting_2_0/Views/EntryVC.swift
@@ -5,14 +5,14 @@
 //  Created by Павел Кузин on 06.09.2020.
 //  Copyright © 2020 Павел Кузин. All rights reserved.
 //
-//
-//import UIKit
-//
-//class EntryVC: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//}
+
+import UIKit
+
+class EntryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Knitting_2_0Tests/ViewModelTests/EntryVMTests.swift
@@ -0,0 +1,33 @@
+//
+//  EntryVMTests.swift
+//  Knitting_2_0Tests
+//
+//  Created by Павел Кузин on 08.09.2020.
+//  Copyright © 2020 Павел Кузин. All rights reserved.
+//
+
+import XCTest
+
+class EntryVMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
